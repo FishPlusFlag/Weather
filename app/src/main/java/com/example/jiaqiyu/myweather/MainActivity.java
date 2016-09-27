014a486a2646b9564880e8ecd38488a999fe4e98
YuJiaqi <jiaqiyu0828@163.com>
2016-09-27 19:32:41 +0800

    init project

---
+++ app/src/main/java/com/example/jiaqiyu/myweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jiaqiyu.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jiaqi Yu on 2016/9/27.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
